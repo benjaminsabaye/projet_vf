b861ffea5efb7918785e2a1aa8a606c75a023d41
Benjamin Sabaye <sh.benjamin02@gmail.com>
2024-11-07 10:33:28 +0100

    v11

---
+++ tp6/identification.js
@@ -0,0 +1,21 @@
+function login() {
+
+    var login = document.getElementById("t1").value;
+    var password = document.getElementById("h1").value;
+    if(login == "admin" && password == "admin")
+    {
+        window.location.href="ok.html";
+    }
+    else {
+        alert("Identifiants non reconnus");
+        window.location.href="non.html";
+    }
+
+}
+
+function retour() {
+
+    document.getElementById("t1").reset();
+    document.getElementById("h1").reset();
+
+}
